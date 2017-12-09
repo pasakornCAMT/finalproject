9c9c71a362dde4031adc9c9c7cad79adca3899b3
Pasakorn Kunchai <pasakorn_kunchai@cmu.ac.th>
2017-12-09 20:13:19 +0700

    setup add-product

--- ng-app/src/app/products/add/add.component.ts
+++ ng-app/src/app/products/add/add.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {ProductDataDbService} from '../../service/product-data-db.service';
+import {Router} from '@angular/router';
+import {Product} from '../product';
 
 @Component({
   selector: 'app-add',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add.component.css']
 })
 export class AddComponent implements OnInit {
-
-  constructor() { }
+  product: any = {};
+  constructor(private productDataDbService: ProductDataDbService, private router: Router) { }
 
   ngOnInit() {
+    this.product = new Product();
   }
+
+  //@ViewChild('fileInput') inputEl: ElementRef;
+
+  /*addProduct(product: Product){
+    let result: Product;
+    console.log(this.product);
+    let inputEl: HTMLInputElement = this.inputEl.nativeElement;
+    this.productDataDbService.addProduct(product,inputEl.files.item(0))
+      .subscribe(resultProduct =>{
+        result = resultProduct;
+        if(result != null){
+          this.router.navigate(['/list']);
+        }else {
+          alert('Errorin adding the product');
+        }
+    })
+  }*/
 
 }
